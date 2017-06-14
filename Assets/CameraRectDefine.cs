f45323e19f6891a568da421984d9108eb0ae478f
Maxwell Foley <maxwellfoley@wustl.edu>
2017-06-13 23:25:01 -0500

    added gitignore

---
+++ Assets/CameraRectDefine.cs
@@ -0,0 +1,25 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class CameraRectDefine : MonoBehaviour {
+
+
+	// Use this for initialization
+	void Start () {
+	
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		Camera camera = GetComponent<Camera>();
+
+		Rect rect = camera.rect;
+
+		rect.width = Screen.width;
+		rect.height = Screen.height;
+		rect.x = 0;
+		rect.y = 0;
+
+		camera.rect = rect;
+	}
+}
