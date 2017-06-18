d13a5ba0d16b0d8885440120d7aed5b8e002f70e
Maxwell Foley <maxwellfoley@wustl.edu>
2017-06-18 16:38:52 -0500

    enemies shoot not all at the same time

--- Assets/ShootRegularly.cs
+++ Assets/ShootRegularly.cs
@@ -6,16 +6,18 @@ public class ShootRegularly : MonoBehaviour {
 	int counter;
 	public Bullet bullet;
 	public int frequency;
+	int offset;
 
 	// Use this for initialization
 	void Start () {
 		counter = 0;
+		offset = (int)(Random.value*frequency);
 	}
 	
 	// Update is called once per frame
 	void FixedUpdate () {
 		counter++;
-		if (counter % frequency == 0) {
+		if (counter % frequency == offset) {
 			Bullet myBullet = (Bullet)Instantiate (bullet, transform.position, transform.rotation);
 			myBullet.direction = new Vector2 (0, -1);
 			myBullet.speed = 0.05f;
