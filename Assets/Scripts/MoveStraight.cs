ed882b9585ed446f8d5342a709fc3553cdbff6bc
Maxwell Foley <maxwellfoley@wustl.edu>
2017-07-05 20:00:15 -0500

    sorted files

--- Assets/Scripts/MoveStraight.cs
+++ Assets/Scripts/MoveStraight.cs
@@ -5,21 +5,14 @@ public class Bullet : MonoBehaviour {
 
 	public Vector2 direction; 
 	public float speed; 
-	int counter;
 
 	// Use this for initialization
 	void Start () {
-		counter = 0;
 	}
 	
 	// Update is called once per frame
 	void FixedUpdate () {
 		transform.Translate (new Vector3 (direction.x * speed, direction.y*speed, 0));
-		counter++;
-		/*
-		if (counter > 100) {
-			Object.Destroy (this.gameObject);
-		}*/
 	}
 
 
